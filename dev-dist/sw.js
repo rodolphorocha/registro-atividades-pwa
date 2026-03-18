2b55563b26d2d5bf6768224a930c9c436f4bc27e
rodolphorocha <rodolpho.r2008@gmail.com>
2026-03-18 16:11:10 -0300

    FEAT: Mensagem de offline

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.oehkst764f4"
+    "revision": "0.658cpl0tu2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
